da05c45704657c5dbd84a1b788313aadec4ebcbd
unit-404 <conway24b@gmail.com>
2024-10-26 22:24:10 +0700

    extension packer

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,5 @@
+call npm run build
 call openssl genrsa -out ./pack/math.pem 2048
 call crx3 ./dist/ -p ./pack/math.pem -o ./pack/math.crx
 call web-ext build --source-dir=./dist/ --artifacts-dir=./pack/ --overwrite-dest=true -n math.xpi
-call web-ext sign --source-dir=./dist/ --api-key=./pack/math.pem --api-secret=./pack/math.pem --channel unlisted
 pause
